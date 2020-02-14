3ec857f3655977329912f19a21ecd4e2d18dfd23
Elizabeth Karen <elizabeth.michael.karen@gmail.com>
2020-02-14 22:57:53 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,16 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  if array.include? value_to_find
+    return array.index(value_to_find)
+  else return nil
+  end
 end
 
 def find_max_value(array)
+  array.max
   # Add your solution here
 end
 
 def find_min_value(array)
+  array.min
   # Add your solution here
 end
