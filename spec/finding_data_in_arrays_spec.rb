650bb56d42bc63079930acb8a7a813ad94e89781
Julian Pormentilla <julian.pormentilla@gmail.com>
2020-02-25 02:09:41 +0000

    Done.

--- spec/finding_data_in_arrays_spec.rb
+++ spec/finding_data_in_arrays_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 require 'pry'
 
-describe 'Finding data' do
+describe 'Finding data' do 
 
   scale = [2,3,4,5]
   scale_2 = [6,5,4,3]
--- thinking_space.rb
+++ thinking_space.rb
@@ -20,15 +20,4 @@ def find_element_index(array, value_to_find)
 
 end
 
-def find_max_value(array) 
-  
-  return array.max 
-  
-end  
-
-def find_min_value(array)
-
-  return array.min 
-
-end
-
+p find_element_index(zig_zag, 1000)  
